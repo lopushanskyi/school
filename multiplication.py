6694e6dbaf02ab1a18f66c76be1f429366e07bbf
lopuszanski <61354513+lopuszanski@users.noreply.github.com>
2020-03-24 15:04:14 +0200

    Added random and while quiz

--- multiplication.py
+++ multiplication.py
@@ -1,8 +1,19 @@
-x = int(input('Введіть множник 1: '))
-y = int(input('Введіть множник 2: '))
+import random
 
-print('Результат множення:')
+r1_1 = 1
+r1_2 = 10
+r1 = random.randint(r1_1, r1_2)
 
-for num in range(1,y+1):
-    z = x * num
-    print(f'{x} x {num} = {z}')
+r2_1 = 1
+r2_2 = 10
+r2 = random.randint(r2_1, r2_2)
+
+answer = int(input(f'Скільки буде {r1} помножити на {r2}: '))
+
+r3 = r1 * r2
+
+while answer != r3:
+    answer = int(input(f'Скільки буде {r1} помножити на {r2}: '))
+    continue
+else:
+    print('you won')
